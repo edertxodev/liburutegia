3166b14b14cbf5405db156644a089a26d1100a48
Edertxo <eferreirate15dw@ikzubirimanteo.com>
2016-03-18 08:40:09 +0000

    Añadida opción que elimina las imagenes antiguas de la carpeta tmp

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -8,12 +8,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+  # Después de guardar la imagen, elimina el archivo temporal de la antigua
+  after :store, :delete_old_tmp_file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  
+  def delete_old_tmp_file(dummy)
+    @old_tmp_file.try :delete
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
