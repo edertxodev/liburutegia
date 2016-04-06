de7ace15d317b97fa5c08c754b3b35a86801a81e
Edertxo <eferreirate15dw@ikzubirimanteo.com>
2016-04-06 18:37:44 +0000

    Crear validación al crear o editar un nuevo libro

--- app/models/libro.rb
+++ app/models/libro.rb
@@ -11,4 +11,17 @@ class Libro < ActiveRecord::Base
         self.all
       end
     end
+    
+    # Validamos los datos antes de crear o modificar un libro
+    
+      # Titulo es obligatorio
+      validates :titulo, presence: { message: "es obligatorio" }
+      # Autor es obligatorio
+      validates :autor, presence: { message: "es obligatorio" }
+      # Género es obligatorio
+      validates :genero, presence: { message: "es obligatorio" }
+      # ISBN es obligatorio
+      validates :isbn, presence: { message: "es obligatorio" }
+      # La cantidad solo puede ser un número entero y no puede ser menor o igual a 0
+      validates :cantidad, presence: { message: "es obligatoria" }, :numericality => { :greater_than => 0, only_integer: true, message: "debe ser mayor que 0" }
 end
