d75dac73d3858580fa4525d2cde54f2459f4f7fb
Edertxo <eferreirate15dw@ikzubirimanteo.com>
2016-05-17 06:18:47 +0000

    Preparada la aplicación para Producción

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510080600) do
+ActiveRecord::Schema.define(version: 20160516105520) do
 
   create_table "comentarios", force: :cascade do |t|
     t.integer  "user_id"
