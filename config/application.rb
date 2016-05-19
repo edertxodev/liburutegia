5e8bea0a27ac49c926fed6b7cf434612f9791cc4
Edertxo <eferreirate15dw@ikzubirimanteo.com>
2016-05-19 06:40:17 +0000

    Borrado el texto README

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Liburutegia
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:eus, :es]
     config.i18n.default_locale = :eus
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
